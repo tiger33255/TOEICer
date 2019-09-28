8e67b6637665baf4710331f813a919f748eabc88
Taiga Yoshizaki <tiger.33255@gmail.com>
2019-09-28 16:17:08 +0900

    change body colum in article table

--- db/migrate/20190911063420_create_articles.rb
+++ db/migrate/20190911063420_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
 
       t.string :title, index: true
-      t.text :body, index: true
+      t.text :body, index: true, :limit =>5000
       t.string :article_image_id, index: true
 
       t.timestamps
